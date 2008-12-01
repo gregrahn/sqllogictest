38db2743263d5eab1bf0f2e2d5c5134f8b81327d
shaneh <shaneh>
2008-12-01 04:54:59 +0000

    Broke out SQLite interface; Added ODBC3 interface; Minor bug fixes.

---
+++ src/slt_odbc3.c
@@ -0,0 +1,504 @@
+/*
+** Copyright (c) 2008 D. Richard Hipp
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public
+** License version 2 as published by the Free Software Foundation.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** General Public License for more details.
+** 
+** You should have received a copy of the GNU General Public
+** License along with this library; if not, write to the
+** Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+** Boston, MA  02111-1307, USA.
+**
+** Author contact information:
+**   drh@hwaci.com
+**   http://www.hwaci.com/drh/
+**
+*******************************************************************************
+** Here begins the implementation of the ODBC3 DbEngine object.
+**
+** This DbEngine expects an ODBC3 DSN named 'sqllogictest'
+** and a database named 'slt' available through that DSN to exist.
+** The DSN should be accessible to the current user.
+**
+*/
+#ifdef WIN32
+#include <windows.h>
+#endif
+#define SQL_NOUNICODEMAP
+#include <sql.h>
+#include <sqlext.h>
+
+
+#define SLT_DSN "sqllogictest"
+#define SLT_DB  "slt"
+
+
+/* forward prototypes */
+static int ODBC3Statement(
+  void *pConn,                /* Connection created by xConnect */
+  const char *zSql            /* SQL statement to evaluate */
+);
+
+
+/*
+** Structure used to hold handles needed for ODBC3 connection.
+*/
+typedef struct ODBC3_Handles ODBC3_Handles;
+struct ODBC3_Handles {
+  SQLHENV env;
+  SQLHDBC dbc;
+};
+
+/*
+** Utility function to display the details of an ODBC3 error.
+*/
+static void ODBC3_perror(char *fn,
+                         SQLHANDLE handle,
+                         SQLSMALLINT type)
+{
+  SQLSMALLINT i = 0;
+  SQLINTEGER native;
+  SQLCHAR state[ 7 ];
+  SQLCHAR text[256];
+  SQLSMALLINT len;
+  SQLRETURN ret;
+
+  do
+  {
+    ret = SQLGetDiagRec(type, 
+                        handle, 
+                        ++i, 
+                        state, 
+                        &native, 
+                        text,
+                        sizeof(text), 
+                        &len );
+    if (SQL_SUCCEEDED(ret))
+    {
+      fprintf(stderr,
+              "%s:%s:%ld:%ld:%s\n", fn, state, (long)i, (long)native, text);
+    }
+  }
+  while( ret == SQL_SUCCESS );
+}
+
+/*
+** Structure used to accumulate a result set.
+*/
+typedef struct ODBC3_resAccum ODBC3_resAccum;
+struct ODBC3_resAccum {
+  char **azValue;   /* Array of pointers to values, each malloced separately */
+  int nAlloc;       /* Number of slots allocated in azValue */
+  int nUsed;        /* Number of slots in azValue used */
+};
+
+/*
+** Append a value to a result set.  zValue is copied into memory obtained
+** from malloc.  Or if zValue is NULL, then a NULL pointer is appended.
+*/
+static void ODBC3_appendValue(ODBC3_resAccum *p, const char *zValue){
+  char *z;
+  if( zValue ){
+#ifdef WIN32
+    z = _strdup(zValue);
+#else
+    z = strdup(zValue);
+#endif
+    if( z==0 ){
+      fprintf(stderr, "out of memory at %s:%d\n", __FILE__, __LINE__);
+      exit(1);
+    }
+  }else{
+    z = 0;
+  }
+  if( p->nUsed>=p->nAlloc ){
+    char **az;
+    p->nAlloc += 200;
+    az = realloc(p->azValue, p->nAlloc*sizeof(p->azValue[0]));
+    if( az==0 ){
+      fprintf(stderr, "out of memory at %s:%d\n", __FILE__, __LINE__);
+      exit(1);
+    }
+    p->azValue = az;
+  }
+  p->azValue[p->nUsed++] = z;
+}
+
+/*
+** Drop all tables from the database on the current connection.
+** This utility function goes to great lengths to ensure 
+** only tables in the test database are dropped. 
+*/
+static int ODBC3_dropAllTables(ODBC3_Handles *pODBC3conn)
+{
+  int rc = 0;
+  SQLRETURN ret;       /* ODBC API return status */
+  SQLSMALLINT columns; /* number of columns in result-set */
+  ODBC3_resAccum res;          /* query result accumulator */
+  SQLUSMALLINT i;
+  char zSql[512];
+  SQLHSTMT stmt = SQL_NULL_HSTMT;
+
+  /* zero out accumulator structure */
+  memset(&res, 0, sizeof(res));
+
+  /* Allocate a statement handle */
+  ret = SQLAllocHandle(SQL_HANDLE_STMT, pODBC3conn->dbc, &stmt);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLAllocHandle", pODBC3conn->dbc, SQL_HANDLE_DBC);
+    return 1;
+  }
+
+  /* Retrieve a list of tables */
+  ret = SQLTables(stmt, NULL, 0, NULL, 0, NULL, 0, "TABLE", SQL_NTS);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLTables", stmt, SQL_HANDLE_STMT);
+    rc = 1;
+  }
+  
+  if( !rc ){
+    /* How many columns are there */
+    SQLNumResultCols(stmt, &columns);
+    if( columns != 5 ){
+      /* Non-standard result set.  Could be non-standard ODBC
+      ** driver, or we're looking at wrong DB.  Return an 
+      ** error and force them to fix this by hand. 
+      ** We don't want to accidentally delete something important. */
+      fprintf(stderr, 
+              "result set of tables has wrong number of columns: %ld\n",
+              (long)columns);
+      rc = 1;
+    }
+  }
+
+  if( !rc ){
+    /* Loop through the rows in the result-set */
+    do {
+      ret = SQLFetch(stmt);
+      if (SQL_SUCCEEDED(ret)) {
+        /* Loop through the columns in the row */
+        for( i=1; i<=columns; i++ ){
+          SQLINTEGER indicator;
+          char zBuffer[512];
+          /* retrieve column data as a string */
+          ret = SQLGetData(stmt, 
+                           i, SQL_C_CHAR,
+                           zBuffer, sizeof(zBuffer), 
+                           &indicator);
+          if (SQL_SUCCEEDED(ret)) {
+            /* Handle null columns */
+            if (indicator == SQL_NULL_DATA){
+              strcpy(zBuffer, "NULL");
+            } else if( *zBuffer == '\0' ) {
+              strcpy(zBuffer, "(empty)");
+            }
+            /* add it to the result list */
+            ODBC3_appendValue(&res, zBuffer);
+          }
+        } /* end for i */
+      }
+    } while (SQL_SUCCEEDED(ret) || (ret ==  SQL_SUCCESS_WITH_INFO));
+  }
+  
+  if( stmt != SQL_NULL_HSTMT ){
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+  }
+
+  if( !rc ){
+    /* for each valid table found, drop it */
+    for( i=0; !rc && (i+4<res.nUsed); i+=5 ){
+      if(    (0 == strcmp(res.azValue[i], SLT_DB))
+          && (0 == strcmp(res.azValue[i+1], "dbo"))
+          && (0 == strcmp(res.azValue[i+3], "TABLE"))
+          && (0 == strcmp(res.azValue[i+4], "NULL")) ){
+        sprintf(zSql, "DROP TABLE %s", res.azValue[i+2]);
+        rc = ODBC3Statement(pODBC3conn, zSql);
+      }
+    }
+  }
+  
+  return rc;
+}
+
+
+/*
+** This routine is called to open a connection to a new, empty database.
+** The zConnectStr argument is the value of the -connection command-line
+** option.  This is intended to contain information on how to connect to
+** the database engine.  The zConnectStr argument will be NULL if there
+** is no -connection on the command-line.  In the case of ODBC3, the
+** zConnectStr is the DSN to open.
+**
+** An object that describes the newly opened and initialized database
+** connection is returned by writing into *ppConn.
+**
+** This routine returns 0 on success and non-zero if there are any errors.
+*/
+static int ODBC3Connect(
+  void *NotUsed,              /* Argument from DbEngine object.  Not used */
+  const char *zConnectStr,    /* Connection string */
+  void **ppConn               /* Write completed connection here */
+){
+  int rc = 0;
+  SQLRETURN ret; /* ODBC API return status */
+  ODBC3_Handles *pODBC3conn = NULL;
+  char szSqlConnStr[512];
+
+  /* Allocate a structure to hold all of our ODBC3 handles */
+  pODBC3conn = (ODBC3_Handles *)malloc(sizeof(ODBC3_Handles));
+  if( !pODBC3conn ){
+    return 1;
+  }
+  pODBC3conn->env = SQL_NULL_HENV;
+  pODBC3conn->dbc = SQL_NULL_HDBC;
+
+  /* Allocate an environment handle */
+  SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &pODBC3conn->env);
+  /* We want ODBC 3 support */
+  SQLSetEnvAttr(pODBC3conn->env, SQL_ATTR_ODBC_VERSION, (void *) SQL_OV_ODBC3, 0);
+  /* Allocate a connection handle */
+  SQLAllocHandle(SQL_HANDLE_DBC, pODBC3conn->env, &pODBC3conn->dbc);
+  /* TBD: check all of the above for failures */
+
+  /* Build the connection string.
+  */
+  sprintf(szSqlConnStr, "DSN=" SLT_DSN ";DATABASE=" SLT_DB ";%s", ( zConnectStr ) ? zConnectStr : "");
+
+  /* Open a connection to the new database.
+  */
+  /* TBD: should we use SQLConnect() here? */
+  ret = SQLDriverConnect(pODBC3conn->dbc, 
+                         NULL, 
+                         (SQLCHAR *)szSqlConnStr, 
+                         SQL_NTS,
+                         NULL, 
+                         0, 
+                         NULL,
+                         SQL_DRIVER_COMPLETE);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLDriverConnect", pODBC3conn->dbc, SQL_HANDLE_DBC);
+    rc = 1;
+  }
+  
+  /* Loop over all tables, etc. available in the database and drop them,
+  ** thus resetting it to an empty database.  */
+  if( !rc ){
+    rc = ODBC3_dropAllTables(pODBC3conn);
+  }
+  
+  /* TBD: is there a way to specify synchronous=OFF or equivalent */
+
+  /* store connection info */
+  *ppConn = (void*)pODBC3conn;
+
+  return rc;  
+}
+
+/*
+** Evaluate the single SQL statement given in zSql.  Return 0 on success.
+** return non-zero if any error occurs.
+*/
+static int ODBC3Statement(
+  void *pConn,                /* Connection created by xConnect */
+  const char *zSql            /* SQL statement to evaluate */
+){
+  int rc = 0;
+  SQLRETURN ret; /* ODBC API return status */
+  ODBC3_Handles *pODBC3conn = pConn;
+  SQLHSTMT stmt = SQL_NULL_HSTMT;
+
+  /* Allocate a statement handle */
+  ret = SQLAllocHandle(SQL_HANDLE_STMT, pODBC3conn->dbc, &stmt);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLAllocHandle", pODBC3conn->dbc, SQL_HANDLE_DBC);
+    return 1;
+  }
+
+  ret = SQLExecDirect(stmt, (SQLCHAR *)zSql, SQL_NTS);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLExecDirect", stmt, SQL_HANDLE_STMT);
+    rc = 1;
+  }
+
+  if( stmt != SQL_NULL_HSTMT ){
+    SQLFreeHandle(SQL_HANDLE_STMT, stmt);
+  }
+
+ return rc;
+}
+
+/*
+** This interface runs a query and accumulates the results into an array
+** of pointers to strings.  *pazResult is made to point to the resulting
+** array and *pnResult is set to the number of elements in the array.
+**
+** NULL values in the result set should be represented by a string "NULL".
+** Empty strings should be shown as "(empty)".  Unprintable and
+** control characters should be rendered as "@".
+**
+** Return 0 on success and 1 if there is an error.  It is not necessary
+** to initialize *pazResult or *pnResult if an error occurs.
+*/
+static int ODBC3Query(
+  void *pConn,                /* Connection created by xConnect */
+  const char *zSql,           /* SQL statement to evaluate */
+  const char *zType,          /* One character for each column of result */
+  char ***pazResult,          /* RETURN:  Array of result values */
+  int *pnResult               /* RETURN:  Number of result values */
+){
+  SQLRETURN ret; /* ODBC API return status */
+  ODBC3_Handles *pODBC3conn = pConn;
+  ODBC3_resAccum res;          /* query result accumulator */
+  char zBuffer[512];          /* Buffer to render numbers */
+  SQLSMALLINT columns;        /* number of columns in result-set */
+  SQLHSTMT stmt = SQL_NULL_HSTMT;
+
+  /* zero out accumulator structure */
+  memset(&res, 0, sizeof(res));
+
+  /* Allocate a statement handle */
+  ret = SQLAllocHandle(SQL_HANDLE_STMT, pODBC3conn->dbc, &stmt);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLAllocHandle", pODBC3conn->dbc, SQL_HANDLE_DBC);
+    return 1;
+  }
+
+  ret = SQLExecDirect(stmt, (SQLCHAR *)zSql, SQL_NTS);
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLExecDirect", stmt, SQL_HANDLE_STMT);
+    return 1;
+  }
+
+  /* How many columns are there */
+  SQLNumResultCols(stmt, &columns);
+
+  /* Loop through the rows in the result-set */
+  while( SQL_SUCCEEDED(ret = SQLFetch(stmt)) ){
+      SQLUSMALLINT i;
+      /* Loop through the columns */
+      for(i = 1; i <= columns; i++){
+          SQLINTEGER indicator;
+          /* retrieve column data as a string */
+          ret = SQLGetData(stmt, 
+                           i, 
+                           SQL_C_CHAR,
+                           zBuffer, 
+                           sizeof(zBuffer), 
+                           &indicator);
+          if( SQL_SUCCEEDED(ret) ){
+              char *z;
+              /* Handle null columns */
+              if( indicator == SQL_NULL_DATA ){ 
+                strcpy(zBuffer, "NULL");
+              /* Handle empty columns */
+              } else if( *zBuffer == '\0' ) {
+                strcpy(zBuffer, "(empty)");
+              }
+              ODBC3_appendValue(&res, zBuffer);
+              /* Convert non-printing and control characters to '@' */
+              z = res.azValue[res.nUsed-1];
+              while( *z ){
+                if( *z<' ' || *z>'~' ){ *z = '@'; }
+                z++;
+              }
+          }
+      }
+  }
+  *pazResult = res.azValue;
+  *pnResult = res.nUsed;
+  return 0;
+}
+
+
+/*
+** This interface is called to free the memory that was returned
+** by xQuery.
+**
+** It might be the case that nResult==0 or azResult==0.
+*/
+static int ODBC3FreeResults(
+  void *pConn,                /* Connection created by xConnect */
+  char **azResult,            /* The results to be freed */
+  int nResult                 /* Number of rows of result */
+){
+  int i;
+  for(i=0; i<nResult; i++){
+    free(azResult[i]);
+  }
+  free(azResult);
+  return 0;
+}
+
+
+/*
+** This routine is called to close a connection previously opened
+** by xConnect.
+**
+** This routine may or may not delete the database.  Whichever way
+** it works, steps should be taken to avoid an accumulation of left-over
+** database files.  If the database is deleted here, that is one approach.
+** The other approach is to delete left-over databases in the xConnect
+** method.  The ODBC3 interface takes the latter approach.
+*/
+static int ODBC3Disconnect(
+  void *pConn                 /* Connection created by xConnect */
+){
+  int rc = 0;
+  SQLRETURN ret; /* ODBC API return status */
+  ODBC3_Handles *pODBC3conn = pConn;
+  
+  if ( !pODBC3conn ){
+    return 1;
+  }
+
+  ret = SQLDisconnect(pODBC3conn->dbc);   /* disconnect from driver */
+  if( !SQL_SUCCEEDED(ret) && (ret != SQL_SUCCESS_WITH_INFO) ){
+    ODBC3_perror("SQLDisconnect", pODBC3conn->dbc, SQL_HANDLE_DBC);
+    rc = 1;
+  }
+  
+  if( pODBC3conn->dbc != SQL_NULL_HDBC ){
+    SQLFreeHandle(SQL_HANDLE_DBC, pODBC3conn->dbc);
+  }
+  if( pODBC3conn->env != SQL_NULL_HENV ){
+    SQLFreeHandle(SQL_HANDLE_ENV, pODBC3conn->env);
+  }
+
+  pODBC3conn->env = SQL_NULL_HENV;
+  pODBC3conn->dbc = SQL_NULL_HDBC;
+
+  free(pODBC3conn);
+  return rc;
+}
+
+/*
+** This routine registers the ODBC3 database engine with the main
+** driver.  New database engine interfaces should have a single
+** routine similar to this one.  The main() function below should be
+** modified to call that routine upon startup.
+*/
+void registerODBC3(void){
+  /*
+  ** This is the object that defines the database engine interface.
+  */
+  static const DbEngine ODBC3DbEngine = {
+     "ODBC3",             /* zName */
+     0,                    /* pAuxData */
+     ODBC3Connect,        /* xConnect */
+     ODBC3Statement,      /* xStatement */
+     ODBC3Query,          /* xQuery */
+     ODBC3FreeResults,    /* xFreeResults */
+     ODBC3Disconnect      /* xDisconnect */
+  };
+  sqllogictestRegisterEngine(&ODBC3DbEngine);
+}
+
+/*
+**************** End of the ODBC3 database engine interface *****************
+*****************************************************************************/
--- src/sqllogictest.c
+++ src/sqllogictest.c
@@ -23,257 +23,21 @@
 **
 ** This main driver for the sqllogictest program.
 */
+#if WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
 #include "sqllogictest.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#ifndef WIN32
 #include <unistd.h>
+#endif
 #include <string.h>
 
-/*****************************************************************************
-** Here begins the implementation of the SQLite DbEngine object.
-**
-** Use this interface as a model for other database engine interfaces.
-*/
-#include "sqlite3.h"
-
-/*
-** This routine is called to open a connection to a new, empty database.
-** The zConnectStr argument is the value of the -connection command-line
-** option.  This is intended to contain information on how to connect to
-** the database engine.  The zConnectStr argument will be NULL if there
-** is no -connection on the command-line.  In the case of SQLite, the
-** zConnectStr is the name of the database file to open.
-**
-** An object that describes the newly opened and initialized database
-** connection is returned by writing into *ppConn.
-**
-** This routine returns 0 on success and non-zero if there are any errors.
-*/
-static int sqliteConnect(
-  void *NotUsed,              /* Argument from DbEngine object.  Not used */
-  const char *zConnectStr,    /* Connection string */
-  void **ppConn               /* Write completed connection here */
-){
-  sqlite3 *db;
-  int rc;
-
-  /* If the database filename is defined and the database already exists,
-  ** then delete the database before we start, thus resetting it to an
-  ** empty database.
-  */
-  if( zConnectStr ){
-    unlink(zConnectStr);
-  }
-
-  /* Open a connection to the new database.
-  */
-  rc = sqlite3_open(zConnectStr, &db);
-  if( rc!=SQLITE_OK ){
-    return 1;
-  }
-  sqlite3_exec(db, "PRAGMA synchronous=OFF", 0, 0, 0);
-  *ppConn = (void*)db;
-  return 0;  
-}
-
-/*
-** Evaluate the single SQL statement given in zSql.  Return 0 on success.
-** return non-zero if any error occurs.
-*/
-static int sqliteStatement(
-  void *pConn,                /* Connection created by xConnect */
-  const char *zSql            /* SQL statement to evaluate */
-){
-  int rc;
-  sqlite3 *db;
-
-  db = (sqlite3*)pConn;
-  rc = sqlite3_exec(db, zSql, 0, 0, 0);
-  return rc!=SQLITE_OK;
-}
-
-/*
-** Structure used to accumulate a result set.
-*/
-typedef struct ResAccum ResAccum;
-struct ResAccum {
-  char **azValue;   /* Array of pointers to values, each malloced separately */
-  int nAlloc;       /* Number of slots allocated in azValue */
-  int nUsed;        /* Number of slots in azValue used */
-};
-
-/*
-** Append a value to a result set.  zValue is copied into memory obtained
-** from malloc.  Or if zValue is NULL, then a NULL pointer is appended.
-*/
-static void appendValue(ResAccum *p, const char *zValue){
-  char *z;
-  if( zValue ){
-    z = sqlite3_mprintf("%s", zValue);
-    if( z==0 ){
-      fprintf(stderr, "out of memory at %s:%d\n", __FILE__, __LINE__);
-      exit(1);
-    }
-  }else{
-    z = 0;
-  }
-  if( p->nUsed>=p->nAlloc ){
-    char **az;
-    p->nAlloc += 200;
-    az = sqlite3_realloc(p->azValue, p->nAlloc*sizeof(p->azValue[0]));
-    if( az==0 ){
-      fprintf(stderr, "out of memory at %s:%d\n", __FILE__, __LINE__);
-      exit(1);
-    }
-    p->azValue = az;
-  }
-  p->azValue[p->nUsed++] = z;
-}
-
-/*
-** This interface runs a query and accumulates the results into an array
-** of pointers to strings.  *pazResult is made to point to the resulting
-** array and *pnResult is set to the number of elements in the array.
-**
-** NULL values in the result set should be represented by a string "NULL".
-** Empty strings should be shown as "(empty)".  Unprintable and
-** control characters should be rendered as "@".
-**
-** Return 0 on success and 1 if there is an error.  It is not necessary
-** to initialize *pazResult or *pnResult if an error occurs.
-*/
-static int sqliteQuery(
-  void *pConn,                /* Connection created by xConnect */
-  const char *zSql,           /* SQL statement to evaluate */
-  const char *zType,          /* One character for each column of result */
-  char ***pazResult,          /* RETURN:  Array of result values */
-  int *pnResult               /* RETURN:  Number of result values */
-){
-  sqlite3 *db;                /* The database connection */
-  sqlite3_stmt *pStmt;        /* Prepared statement */
-  int rc;                     /* Result code from subroutine calls */
-  ResAccum res;               /* query result accumulator */
-  char zBuffer[200];          /* Buffer to render numbers */
-
-  memset(&res, 0, sizeof(res));
-  db = (sqlite3*)pConn;
-  rc = sqlite3_prepare_v2(db, zSql, -1, &pStmt, 0);
-  if( rc!=SQLITE_OK ){
-    sqlite3_finalize(pStmt);
-    return 1;
-  }
-  while( sqlite3_step(pStmt)==SQLITE_ROW ){
-    int i;
-    for(i=0; zType[i]; i++){
-      if( sqlite3_column_type(pStmt, i)==SQLITE_NULL ){
-        appendValue(&res, "NULL");
-      }else{
-        switch( zType[i] ){
-          case 'T': {
-            const char *zValue = (const char*)sqlite3_column_text(pStmt, i);
-            char *z;
-            if( zValue[0]==0 ) zValue = "(empty)";
-            appendValue(&res, zValue);
-
-            /* Convert non-printing and control characters to '@' */
-            z = res.azValue[res.nUsed-1];
-            while( *z ){
-              if( *z<' ' || *z>'~' ){ *z = '@'; }
-              z++;
-            }
-            break;
-          }
-          case 'I': {
-            int ii = sqlite3_column_int(pStmt, i);
-            sqlite3_snprintf(sizeof(zBuffer), zBuffer, "%d", ii);
-            appendValue(&res, zBuffer);
-            break;
-          }
-          case 'R': {
-            double r = sqlite3_column_double(pStmt, i);
-            sqlite3_snprintf(sizeof(zBuffer), zBuffer, "%.3f", r);
-            appendValue(&res, zBuffer);
-            break;
-          }
-          default: {
-            sqlite3_finalize(pStmt);
-            fprintf(stderr, "Unknown character in type-string: %c\n", zType[i]);
-            return 1;
-          }
-        }
-      }
-    }
-  }
-  sqlite3_finalize(pStmt);
-  *pazResult = res.azValue;
-  *pnResult = res.nUsed;
-  return 0;
-}
-
-/*
-** This interface is called to free the memory that was returned
-** by xQuery.
-**
-** It might be the case that nResult==0 or azResult==0.
-*/
-static int sqliteFreeResults(
-  void *pConn,                /* Connection created by xConnect */
-  char **azResult,            /* The results to be freed */
-  int nResult                 /* Number of rows of result */
-){
-  int i;
-  for(i=0; i<nResult; i++){
-    sqlite3_free(azResult[i]);
-  }
-  sqlite3_free(azResult);
-  return 0;
-}
-
-/*
-** This routine is called to close a connection previously opened
-** by xConnect.
-**
-** This routine may or may not delete the database.  Whichever way
-** it works, steps should be taken to avoid an accumulation of left-over
-** database files.  If the database is deleted here, that is one approach.
-** The other approach is to delete left-over databases in the xConnect
-** method.  The SQLite interface takes the latter approach.
-*/
-static int sqliteDisconnect(
-  void *pConn                 /* Connection created by xConnect */
-){
-  sqlite3 *db = (sqlite3*)pConn;
-  sqlite3_close(db);
-  return 0;
-}
-
-/*
-** This routine registers the SQLite database engine with the main
-** driver.  New database engine interfaces should have a single
-** routine similar to this one.  The main() function below should be
-** modified to call that routine upon startup.
-*/
-void registerSqlite(void){
-  /*
-  ** This is the object that defines the database engine interface.
-  */
-  static const DbEngine sqliteDbEngine = {
-     "SQLite",             /* zName */
-     0,                    /* pAuxData */
-     sqliteConnect,        /* xConnect */
-     sqliteStatement,      /* xStatement */
-     sqliteQuery,          /* xQuery */
-     sqliteFreeResults,    /* xFreeResults */
-     sqliteDisconnect      /* xDisconnect */
-  };
-  sqllogictestRegisterEngine(&sqliteDbEngine);
-}
-
-/*
-**************** End of the SQLite database engine interface *****************
-*****************************************************************************/
-
+#include "slt_sqlite.c"
+#include "slt_odbc3.c"
 
 /*
 ** An array of registered database engines
@@ -291,7 +55,7 @@ void sqllogictestRegisterEngine(const DbEngine *p){
     fprintf(stderr, "out or memory - line %d\n", __LINE__);
     exit(1);
   }
-  apEngine[nEngine-1] = p;
+  apEngine[nEngine-1] = (DbEngine *)p;
 }
 
 /*
@@ -356,7 +120,7 @@ static int nextLine(Script *p){
     if( p->len>0 && p->zScript[i-1]=='\r' ){
       p->len--;
       i--;
-      p->zScript[i-1] = 0;
+      p->zScript[i] = 0;
     }
 
     /* If the line consists of all spaces, make it an empty line */
@@ -482,7 +246,7 @@ static int rowCompare(const void *pA, const void *pB){
 int main(int argc, char **argv){
   int verifyMode = 0;                  /* True if in -verify mode */
   const char *zScriptFile = 0;         /* Input script filename */
-  const char *zDbEngine = "SQLite";    /* Name of database engine */
+  const char *zDbEngine = NULL;        /* Name of database engine */
   const char *zConnection = 0;         /* Connection string on DB engine */
   const DbEngine *pEngine = 0;         /* Pointer to DbEngine object */
   int i;                               /* Loop counter */
@@ -502,11 +266,25 @@ int main(int argc, char **argv){
   ** interfaces here
   */
   registerSqlite();
+  registerODBC3();
+
+  /* Report an error if no registered engines
+  */
+  if( nEngine == 0 ){
+    fprintf(stderr, "%s: no registered database engines\n", argv[0]);
+    usage(argv[0]);
+  }
+
+  /* Default to first registered engine 
+  */
+  if( zDbEngine == NULL ){
+    zDbEngine = apEngine[0]->zName;
+  }
 
   /* Scan the command-line and process arguments
   */
   for(i=1; i<argc; i++){
-    int n = strlen(argv[i]);
+    int n = (int)strlen(argv[i]);
     if( strncmp(argv[i], "-verify",n)==0 ){
       verifyMode = 1;
     }else if( strncmp(argv[i], "-engine",n)==0 ){
@@ -676,7 +454,7 @@ int main(int argc, char **argv){
         /* Do no sorting */
       }else if( strcmp(sScript.azToken[2],"rowsort")==0 ){
         /* Row-oriented sorting */
-        nColumn = strlen(sScript.azToken[1]);
+        nColumn = (int)strlen(sScript.azToken[1]);
         qsort(azResult, nResult/nColumn, sizeof(azResult[0])*nColumn,
               rowCompare);
       }else if( strcmp(sScript.azToken[2],"valuesort")==0 ){
