e5cf44d477bd2c56e9cb959eae61d812aa523e85
drh <drh>
2011-09-14 15:48:25 +0000

    Update the built-in SQLite to the latest 3.7.8 beta.

--- src/sqlite3.h
+++ src/sqlite3.h
@@ -109,7 +109,7 @@ extern "C" {
 */
 #define SQLITE_VERSION        "3.7.8"
 #define SQLITE_VERSION_NUMBER 3007008
-#define SQLITE_SOURCE_ID      "2011-08-26 11:25:02 1dada5158215d1816edb69ff2610f9d2259ce19d"
+#define SQLITE_SOURCE_ID      "2011-09-14 13:41:40 a1f3aeeb0988c848d40ce8f6da6e902935a997e2"
 
 /*
 ** CAPI3REF: Run-Time Library Version Numbers
