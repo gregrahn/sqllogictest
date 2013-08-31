6951245c4827004c597845b999ff80a78376a88b
mistachkin <mistachkin>
2013-08-31 20:22:38 +0000

    Rename the PostgreSQL DSN in the test batch file for consistency.

--- src/run-all-pgsql.bat
+++ src/run-all-pgsql.bat
@@ -1,1 +1,1 @@
-for /R ..\test %%i in (*.test) do sqllogictest -odbc "DSN=slt.psql;UID=slt;PWD=slt;" -verify %%i
+for /R ..\test %%i in (*.test) do sqllogictest -odbc "DSN=slt.pgsql;UID=slt;PWD=slt;" -verify %%i
